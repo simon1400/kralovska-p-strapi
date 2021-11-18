45f3422b8e0ca662288159f5cfc7dc8583358181
Your Name <>
2021-11-18 20:24:04 +0100

    fixed editor

--- app/extensions/content-manager/admin/src/components/CKEditor/index.js
+++ app/extensions/content-manager/admin/src/components/CKEditor/index.js
@@ -7,18 +7,47 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   .ck-editor__main {
     min-height: 200px;
+    max-height: 400px;
     > div {
       min-height: 200px;
+      max-height: 400px;
     }
   }
 `;
+
+const configuration = {
+  toolbar: [
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    'link',
+    'bulletedList',
+    'numberedList',
+    '|',
+    'indent',
+    'outdent',
+    '|',
+    'blockQuote',
+    'insertTable',
+    'mediaEmbed',
+    'undo',
+    'redo',
+  ],
+};
 
 const Editor = ({ onChange, name, value }) => {
   return (
     <Wrapper>
       <CKEditor
         editor={ClassicEditor}
+        config={configuration}
         data={value}
+        onReady={editor => {
+          if (value) {
+            editor.setData(value);
+          }
+        }}
         onChange={(event, editor) => {
           const data = editor.getData();
           onChange({ target: { name, value: data } });

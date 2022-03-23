7d525bdf9bfbdead705fc19e14c029729bc35146
simon1400 <pechunka11@gmail.com>
2022-03-23 16:20:42 +0100

    initial new repo

---
+++ app/config/functions/cron.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Cron config that gives you an opportunity
+ * to run scheduled jobs.
+ *
+ * The cron format consists of:
+ * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
+ *
+ * See more details here: https://strapi.io/documentation/developer-docs/latest/setup-deployment-guides/configurations.html#cron-tasks
+ */
+
+module.exports = {
+  '0 */12 * * *': () => {
+    strapi.plugins.sitemap.services.sitemap.createSitemap();
+  },
+};
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '545b0746-5399-4344-9d0a-8dfdcdea1805'
+};

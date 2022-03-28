0ebfd17d6cee3c7fac822d94c41c763e64729d96
Your Name <>
2022-03-28 19:13:00 +0200

    add cloudary

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -1,11 +1,16 @@
-module.exports = {
+module.exports = ({ env }) => ({
   upload: {
-    breakpoints: {
-      xlarge: 1920,
-      large: 1000,
-      medium: 750,
-      small: 500,
-      xsmall: 64
-    }
-  }
-}
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+})
